ec8a9641e5b471fdebbe1efb0ed8ebabdff3d30f
Chris Martine <cmmartine@gmail.com>
2023-01-18 14:05:36 -0500

    Broke up flight index into partials, added form flight_choice for use after Bookings is created

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,7 +8,6 @@ class FlightsController < ApplicationController
     departure_airport = params[:departure_airport_id]
     arrival_airport = params[:arrival_airport_id]
     date = params[:flight_date]
-
     @searched_flights = Flight.where(departure_airport_id: departure_airport, arrival_airport_id: arrival_airport, date: date).first(5)
   end
 end
