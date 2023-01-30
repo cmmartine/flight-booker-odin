e30cd8d77e6191100cf9b67f52d0c9ed5d7a9715
Chris Martine <cmmartine@gmail.com>
2023-01-30 12:45:14 -0500

    Install RSpec, Capybara, run tests for flight choice

---
+++ spec/features/user_searches_for_flight_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "User searches for flights" do
+  before do
+    Rails.application.load_seed
+  end
+  scenario "they see flights that correspond to their choices if available" do
+
+    visit root_path
+    select('NYC', from: "departure_airport_id")
+    select('EWR', from: 'arrival_airport_id')
+    select('2', from: 'number_of_passengers')
+    select('2023-01-28', from: 'flight_date')
+    click_on "Search"
+    expect(page).to have_content("Departing from: 1")
+  end
+end
